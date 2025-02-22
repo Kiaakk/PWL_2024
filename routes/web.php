e11f62044783109907ca074ca24ad415007e498a
Kiaakk <krisna.wiliam26@gmail.com>
2025-02-22 19:46:52 +0700

    Minggu 2 - Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'NIM : 2341720253 <br> Nama : Hizkia Elsadanta';
+});
+
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postid, $commentid) {
+    return 'Pos ke-' .$postid. " Komentar ke-" .$commentid;
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman Artikel dengan ID ' .$id ;
+});
+
+Route::get('/user/{name?}', function($name='John') {
+    return 'Nama saya ' .$name;
+});
+
+Route::get('/user/profile', function() {
+    //
+})->name('profile');
